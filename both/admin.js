f91160502f453818d217075ff47f0a8fb2b17b4a
Pietro Abate <pietro.abate@pps.univ-paris-diderot.fr>
2017-10-11 16:55:08 +0200

    add generic conf for admin module

---
+++ both/admin.js
@@ -0,0 +1,9 @@
+AdminConfig = {
+  name: 'My App',
+  adminEmails: ['admin@example.com'],
+  collections: {
+    "Volunteers.Collections.Team": {label: "Team"},
+    "Volunteers.Collections.Division": {label: "Division"},
+    "Volunteers.Collections.Department": {label: "Department"},
+  },
+};
