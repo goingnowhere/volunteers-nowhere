7935729afdfcec42b408f6cbad0762408eec9fd7
Pietro <pietro.abate@inria.fr>
2018-04-08 10:18:28 +0200

    Refine volunteer form

--- imports/fixtures/profile-form.js
+++ imports/fixtures/profile-form.js
@@ -9,55 +9,108 @@ const form = [
 		"type": "textarea",
 		"label": "Bio",
 		"className": "form-control",
-		"name": "textarea-1511695651949"
+		"name": "textarea-1511695651949",
+		"rows": "5"
 	},
 	{
-		"type": "select",
-		"label": "lalal",
-		"className": "form-control",
-		"name": "select-1516830576815",
+		"type": "radio-group",
+		"label": "Gender",
+		"description": "This info is used in particular shifts where a specific gender is required.",
+		"inline": true,
+		"name": "radio-group-1523175199854",
 		"values": [
 			{
-				"label": "Option 1",
-				"value": "option-1",
-				"selected": true
+				"label": "Male",
+				"value": "male"
 			},
 			{
-				"label": "Option 2",
-				"value": "option-2"
+				"label": "Female",
+				"value": "female"
 			},
 			{
-				"label": "Option 3",
-				"value": "option-3"
+				"label": "In Between",
+				"value": "between"
 			}
 		]
 	},
 	{
-		"type": "date",
-		"required": true,
-		"label": "Arrival Date",
-		"className": "form-control",
-		"name": "date-1516817303811"
-	},
-	{
 		"type": "checkbox-group",
-		"label": "Dietary Restrictions",
+		"label": "Grave Allergies",
+		"description": "We have to take you to the hospital.",
 		"inline": true,
 		"name": "checkbox-group-1516817336171",
 		"values": [
 			{
-				"label": "Celiac",
+				"label": "Gluten/Celiac",
 				"value": "celiac"
 			},
 			{
-				"label": "Gluten intolerant ",
-				"value": "gluten"
+				"label": "Fish/Shellfish",
+				"value": "shellfish"
+			},
+			{
+				"label": "Peanuts/Nuts",
+				"value": "nuts"
+			},
+			{
+				"label": "Tree nuts",
+				"value": "treenuts"
+			},
+			{
+				"label": "Soy",
+				"value": "soy"
+			},
+			{
+				"label": "Egg",
+				"value": "egg"
+			},
+			{
+				"label": "I'm DavidB",
+				"value": "davidb"
 			}
 		]
 	},
 	{
 		"type": "checkbox-group",
-		"label": "Foodwise, are you",
+		"label": "Food Intollerances",
+		"description": "You are not going to die if you come in contacts with traces of these common allergens. There might be contamination. Please be flexible and come talk to us for very special requirements.",
+		"inline": true,
+		"name": "checkbox-group-1523174426198",
+		"values": [
+			{
+				"label": "Intolerance to gluten ",
+				"value": "gluten"
+			},
+			{
+				"label": "Peppers",
+				"value": "peppers"
+			},
+			{
+				"label": "Fish/shellfish",
+				"value": "shellfish"
+			},
+			{
+				"label": "Peanuts/Nuts",
+				"value": "nuts"
+			},
+			{
+				"label": "Egg",
+				"value": "egg"
+			},
+			{
+				"label": "Milk/Lactose",
+				"value": "lactose"
+			},
+			{
+				"label": "Other",
+				"value": "other"
+			}
+		]
+	},
+	{
+		"type": "checkbox-group",
+		"label": "Food Preference",
+		"description": "The local producer we use for meat is not certified hallal   and our kitchen is not kosher.",
 		"name": "checkbox-group-1516823589180",
 		"values": [
 			{
@@ -72,8 +125,20 @@ const form = [
 			{
 				"label": "Vegan",
 				"value": "vegan"
+			},
+			{
+				"label": "Fishetarian ( no meat, but fish ok)",
+				"value": "fish"
 			}
 		]
+	},
+	{
+		"type": "textarea",
+		"label": "Experience as volunteer at Burn?",
+		"description": "When  how ?",
+		"className": "form-control",
+		"name": "textarea-1523175003065",
+		"rows": "3"
 	},
 	{
 		"type": "text",
