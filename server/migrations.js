fded2147668bc8bd092cad786da77ce17cc78bdf
Pietro <pietro.abate@inria.fr>
2018-04-28 19:29:14 +0200

    Make three new admin in production

--- server/migrations.js
+++ server/migrations.js
@@ -1,5 +1,7 @@
 import { EmailForms } from 'meteor/abate:email-forms'
 import { Migrations } from 'meteor/percolate:migrations'
+import { Accounts } from 'meteor/accounts-base'
+import { Roles } from 'meteor/piemonkey:roles'
 
 Migrations.config({
   log: true,
@@ -26,5 +28,18 @@ Migrations.add({
         { name: 'url', description: 'absolute URL of the site' },
       ],
     })
+  },
+})
+
+Migrations.add({
+  version: 2,
+  name: 'Make three admins',
+  up() {
+    const pm = Accounts.findUserByEmail('piemonkey@gmail.com')
+    const dr = Accounts.findUserByEmail('dr.munga@gmail.com')
+    const hc = Accounts.findUserByEmail('hardcastle@goingnowhere.org')
+    Roles.addUsersToRoles(pm._id, 'admin')
+    Roles.addUsersToRoles(dr._id, 'admin')
+    Roles.addUsersToRoles(hc._id, 'admin')
   },
 })
