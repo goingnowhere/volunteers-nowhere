feb9271e4cb737b6e24fc1aa343f6a103d152d7e
Rich <piemonkey@gmail.com>
2019-01-04 23:47:23 +0000

    Upgrade to Meteor 1.8.0.1 - Required pulling in meteor-pages

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ import { check, Match } from 'meteor/check'
 import { MeteorProfile, Volunteers } from '../both/init'
 
 /*  managers, leads and the user itself is authorized to see the picture. */
-Meteor.publish('meteor-user-profiles.ProfilePictures', function publishProfilePictures(userId) {
+Meteor.publish('meteor-user-profiles.ProfilePictures', function publishProfilePictures(userId = this.userId) {
   check(userId, Match.Maybe(String))
   const authUserId = userId || this.userId
   if (authUserId === userId || // the user asking for its picture
