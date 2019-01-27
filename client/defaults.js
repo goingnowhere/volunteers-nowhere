29fd0cf6b54c7754af4e34f4f9d8cac766cfaa69
Rich <piemonkey@gmail.com>
2019-01-27 16:29:23 +0000

    Fix display of error messages for project signup conflicts

--- client/defaults.js
+++ client/defaults.js
@@ -1,6 +1,6 @@
 import { AutoForm } from 'meteor/aldeed:autoform'
 import { i18n } from 'meteor/universe:i18n'
-import * as Bert from 'meteor/themeteorchef:bert'
+import { Bert } from 'meteor/themeteorchef:bert'
 
 Bert.defaults = {
   hideDelay: 4500,
@@ -10,10 +10,20 @@ Bert.defaults = {
 
 AutoForm.addHooks(null, {
   onError(operation, error) {
-    Bert.alert({
-      title: i18n.__('wrong_or_required_field'),
-      message: error.message,
-      type: 'warning',
-    })
+    if (error && error.error === 409 && error.message === 'Double Booking') {
+      Bert.alert({
+        hideDelay: 6500,
+        title: i18n.__('double_booking'),
+        message: i18n.__('double_booking_msg'),
+        type: 'warning',
+        style: 'growl-top-right',
+      })
+    } else {
+      Bert.alert({
+        title: i18n.__('wrong_or_required_field'),
+        message: error.message,
+        type: 'warning',
+      })
+    }
   },
 })
